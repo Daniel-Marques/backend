846c1292e359b54752c691de4695be3e32a78672
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-01 21:51:30 -0300

    Exceptiton not found user

--- src/routers/router_users.py
+++ src/routers/router_users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm.session import Session
 from src.schemas.schema import UserSchema
 from src.infra.sqlalchemy.config.database import get_db
@@ -9,29 +9,32 @@ router = APIRouter()
 
 
 @router.get('/users', status_code=status.HTTP_200_OK, tags=["Users"])
-def index(db: Session = Depends(get_db)):
+async def index(db: Session = Depends(get_db)):
     return UserRepository(db).index()
 
 
 @router.post('/users', tags=["Users"])
-def create(user: UserSchema, db: Session = Depends(get_db)):
+async def create(user: UserSchema, db: Session = Depends(get_db)):
     user_created = UserRepository(db).create(user)
     return user_created
 
 
 @router.put('/users/{id}', tags=["Users"])
-def update(id: int, user: UserSchema, db: Session = Depends(get_db)):
+async def update(id: int, user: UserSchema, db: Session = Depends(get_db)):
     user_updated = UserRepository(db).update(id, user)
     return user
 
 
 @router.get('/users/{user_id}', tags=["Users"])
-def show(user_id: int, db: Session = Depends(get_db)):
-    user_found = UserRepository(db).show(user_id)
-    return user_found
+async def show(user_id: int, db: Session = Depends(get_db)):
+    try:
+        user_found = UserRepository(db).show(user_id)
+        return user_found
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
 
 
 @router.delete('/users/{user_id}', tags=["Users"])
-def destroy(user_id: int, db: Session = Depends(get_db)):
+async def destroy(user_id: int, db: Session = Depends(get_db)):
     UserRepository(db).destroy(user_id)
     return {'message': 'Usuário removido com sucesso.'}
