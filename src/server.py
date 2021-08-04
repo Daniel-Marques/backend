debeb275b0eae3b34cadc8510e0e09044af2a6a8
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-04 09:43:32 -0300

    Fix Cors

--- src/server.py
+++ src/server.py
@@ -4,16 +4,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "https://localhost:3000",
-    "https://front-newmission.herokuapp.com",
-    "http://front-newmission.herokuapp.com"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
