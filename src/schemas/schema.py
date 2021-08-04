94d5b9716131844ef1ef85a7fb0742a54bb016ac
Daniel Marques <daniel.silva.city@gmail.com>
2021-08-04 09:53:04 -0300

    Schema UserSimpleSchema

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class UserSchema(BaseModel):
     id: Optional[str]
     name: str
@@ -18,6 +19,20 @@ class UserSchema(BaseModel):
     created_at: Optional[str]
     updated_at: Optional[str]
 
+class UserSimpleSchema(BaseModel):
+    id: Optional[int] = None
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
 class LoginSchema(BaseModel):
     email: str
     password: str
+
+
+class LoginSuccessSchema(BaseModel):
+    user: UserSimpleSchema
+    access_token: str
